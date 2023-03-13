63563b3c347e3eb5f5665521f007825ef01a6c12
Gaspar <gasparalmaraz.2013@gmail.com>
2023-03-13 11:29:48 -0300

    Update validation.js

--- src/components/Views/Form/validation.js
+++ src/components/Views/Form/validation.js
@@ -5,20 +5,17 @@ export default function validation(inputs){
 
     if(!inputs.username){
         errors.username = "Ingrese un username."
-    }else
-    if(!regexusername.test(inputs.username)){
+    }else if(!regexusername.test(inputs.username)){
         errors.username = "Debe ser un correo electronico.";
-    }else
-    if(inputs.username.length > 35){
+    }else if(inputs.username.length > 35){
         errors.username = "El username no puede tener mas de 35 caraceteres."
     }
+
     if(!inputs.password){
         errors.password = "Ingrese contraseña";
-    }else
-    if (!regexInt.test(inputs.password)) {
+    }else if (!regexInt.test(inputs.password)) {
         errors.password = "La contraseña debe contener al menos un número";
-    }else
-    if(inputs.password.length < 6 || inputs.password.length > 10){
+    }else if(inputs.password.length < 6 || inputs.password.length > 10){
         errors.password = "La contraseña debe ser entre 6 y 10 caracteres."
     }
     return errors;
