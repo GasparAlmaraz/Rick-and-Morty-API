a328c2086e93e6af0cd6d4a75564696511677101
Gaspar <gasparalmaraz.2013@gmail.com>
2023-03-27 14:49:36 -0300

    added styles to login screen

--- front/src/components/Views/Form/Form.jsx
+++ front/src/components/Views/Form/Form.jsx
@@ -2,21 +2,21 @@ import styles from './Form.module.css';
 import { useState } from 'react';
 import validation from './validation';
 
-export default function Form(props){
+export default function Form(props) {
 
-    const [userData, setUserData] = useState({ 
-        username: '', 
-        password: '' 
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
     });
 
     const [errors, setErrors] = useState({
-        username: '', 
-        password: '' 
+        username: '',
+        password: ''
     });
 
     const handleInputChange = (event) => {
         setUserData({
-            ...userData, [event.target.name] : event.target.value,
+            ...userData, [event.target.name]: event.target.value,
         });
     }
 
@@ -25,29 +25,31 @@ export default function Form(props){
         const validationErrors = validation(userData);
         setErrors(validationErrors);
         if (Object.keys(validationErrors).length === 0) {
-        setErrors({});
-        props.login(userData);
+            setErrors({});
+            props.login(userData);
         } else {
-        alert("Debe llenar todos los campos");
-        setErrors(validationErrors);
+            alert("Debe llenar todos los campos");
+            setErrors(validationErrors);
         }
     }
 
-    return(
-        <form className={styles.container} onSubmit={handleSubmit}>
-            <label htmlFor='username'>
-                Username 
-            </label>
-            <input className={errors.username && 'warning'} name='username' type="text" value={userData.username} onChange={handleInputChange}>
-            </input>
-            <p>{errors.username}</p>
-            <label htmlFor='password'>
-                Password 
-            </label>
-            <input className={errors.password && 'warning'} name='password' type="password" value={userData.password} onChange={handleInputChange}>
-            </input>
-            <p>{errors.password}</p>
-            <button type='submit'>LOGIN</button>
-        </form>
+    return (
+        <div className={styles.container}>
+            <form className={styles.formContainer} onSubmit={handleSubmit}>
+                <label htmlFor='username'>
+                    Username
+                </label>
+                <input className={errors.username && 'warning'} name='username' type="text" value={userData.username} onChange={handleInputChange}>
+                </input>
+                <p>{errors.username}</p>
+                <label htmlFor='password'>
+                    Password
+                </label>
+                <input className={errors.password && 'warning'} name='password' type="password" value={userData.password} onChange={handleInputChange}>
+                </input>
+                <p>{errors.password}</p>
+                <button type='submit'>LOGIN</button>
+            </form>
+        </div>
     )
 }
