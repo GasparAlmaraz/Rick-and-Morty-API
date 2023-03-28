41d24a7faf4813ed4824ef3042abf04e7bc42651
GasparAlmaraz <gasparalmaraz.2013@gmail.com>
2023-03-28 15:49:58 -0300

    Homework complete?

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,17 +1,26 @@
 const axios = require('axios');
-const {URL, APIKEY} = process.env;
 
-const getCharById = (req,res) => {
-    
-    const {id} = req.params;
-    const URL ="https://be-a-rym.up.railway.app/api";
-    const APIKEY = "67f45d299611.46234457e8c467c632bb";
-    axios.get(`${URL}/character/${id}?key=${APIKEY}`)
-    .then(response => {
-        const {id, name, species, image, gender} = response.data
-        res.status(200).json({id, name, species, image, gender});
-    })
-    .catch(error => res.status(500).json({error: error.message}));
+const getCharById = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const URL = "https://be-a-rym.up.railway.app/api";
+        const APIKEY = "67f45d299611.46234457e8c467c632bb";
+        const response = await axios.get(`${URL}/character/${id}?key=${APIKEY}`)
+
+        const obj = {
+            id: response.data.id,
+            name: response.data.name,
+            species: response.data.species,
+            image: response.data.image,
+            gender: response.data.gender
+        }
+        res.status(200).json(obj);
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
+
+
 
 module.exports = getCharById;
--- back/src/controllers/getCharDetail.js
+++ back/src/controllers/getCharDetail.js
@@ -3,15 +3,26 @@ const axios = require('axios');
 const URLBASE = "https://be-a-rym.up.railway.app/api";
 const APIKEY = "67f45d299611.46234457e8c467c632bb"
 
-const getCharDetail = (req, res) => {
+const getCharDetail = async (req, res) => {
     
-    const {id} = req.params;
-    axios.get(`${URLBASE}/character/${id}?key=${APIKEY}`)
-    .then(response => {
-        const {id, name, species, image, gender, origin} = response.data
-        res.status(200).json({id, name, species, image, gender, origin});
-    })
-    .catch(error => res.status(500).json({error: error.message}));
+    try {
+        const { id } = req.params;
+        const URL = "https://be-a-rym.up.railway.app/api";
+        const APIKEY = "67f45d299611.46234457e8c467c632bb";
+        const response = await axios.get(`${URL}/character/${id}?key=${APIKEY}`)
+
+        const obj = {
+            id: response.data.id,
+            name: response.data.name,
+            species: response.data.species,
+            image: response.data.image,
+            gender: response.data.gender,
+            origin: response.data.origin
+        }
+        res.status(200).json(obj);
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 }
 
 module.exports = getCharDetail;
